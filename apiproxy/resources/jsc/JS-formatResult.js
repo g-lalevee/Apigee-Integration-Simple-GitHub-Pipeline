7ef8da7f8cecdf722cb440f56c4a13b416995478
g-lalevee <lalevee@google.com>
2022-04-27 00:06:42 +0200

    v4

--- apiproxy/resources/jsc/JS-formatResult.js
+++ apiproxy/resources/jsc/JS-formatResult.js
@@ -1,8 +1,4 @@
 var combined = context.getVariable('integrationresponse.combinedresponse');
-var entity = context.getVariable('entity');
 combined = JSON.parse(combined);
-combined = combined[0].replace(/\s/g, '');
-var _json = JSON.stringify(combined).replace(/\\/g,'');
-_json = _json.substring(1).slice(0,-1);
-
+var _json = JSON.stringify(combined[Object.keys(combined)[0]] );
 context.setVariable('response.content', _json);
